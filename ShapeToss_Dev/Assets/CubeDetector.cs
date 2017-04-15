ba6f19c595a3178b73c34c07bd08f0ba076de245
alcamore <alcamore@gmail.com>
2017-04-15 15:39:28 -0700

    Added scoring & victory condition/text to ShapeToss

---
+++ ShapeToss_Dev/Assets/CubeDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeDetector : MonoBehaviour {
+	public Text cubeText;
+
+	private int cubeScore;
+	private int numCubes;
+
+	// Use this for initialization
+	void Start () {
+		cubeScore = 0;
+		numCubes = 7;
+		updateText ();
+	}
+		
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Cube")) {
+			cubeScore++;
+			updateText ();
+		}
+	}
+
+	void updateText() {
+		cubeText.text = "Cubes: " + cubeScore + "/" + numCubes;
+	}
+}
