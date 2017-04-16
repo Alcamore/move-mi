ba4a00deef7bf6c52fb60820b34e232e8e2d8e38
alcamore <alcamore@gmail.com>
2017-04-15 17:49:20 -0700

    Final push

--- ElectronApp/app/js/index.js
+++ ElectronApp/app/js/index.js
@@ -11,19 +11,19 @@ document.getElementById('gameButton1').onclick = function () {
       console.log(data.toString());
   });
 };
-// document.getElementById('gameButton2').onclick = function () {
-//   var child = require('child_process').execFile;
-//   var executablePath = "C:\\Users\\JustinShort\\Documents\\move-mi-master\\LeapTest\\TestBuild.exe";
-//
-//   child(executablePath, function(err, data) {
-//       if(err){
-//          console.error(err);
-//          return;
-//       }
-//
-//       console.log(data.toString());
-//   });
-// };
+document.getElementById('gameButton2').onclick = function () {
+  var child = require('child_process').execFile;
+  var executablePath = __dirname + "\\SlidingPuzzle.exe";
+
+  child(executablePath, function(err, data) {
+      if(err){
+         console.error(err);
+         return;
+      }
+
+      console.log(data.toString());
+  });
+};
  document.getElementById('gameButton3').onclick = function () {
    var child = require('child_process').execFile;
    var executablePath = __dirname + "\\ShapeToss.exe";
@@ -37,16 +37,16 @@ document.getElementById('gameButton1').onclick = function () {
        console.log(data.toString());
    });
  };
-// document.getElementById('gameButton4').onclick = function () {
-//   var child = require('child_process').execFile;
-//   var executablePath = "C:\\Users\\JustinShort\\Documents\\move-mi-master\\LeapTest\\TestBuild.exe";
-//
-//   child(executablePath, function(err, data) {
-//       if(err){
-//          console.error(err);
-//          return;
-//       }
-//
-//       console.log(data.toString());
-//   });
-// };
+document.getElementById('gameButton4').onclick = function () {
+  var child = require('child_process').execFile;
+  var executablePath = __dirname + "\\FightClub.exe";
+
+  child(executablePath, function(err, data) {
+      if(err){
+         console.error(err);
+         return;
+      }
+
+      console.log(data.toString());
+  });
+};
